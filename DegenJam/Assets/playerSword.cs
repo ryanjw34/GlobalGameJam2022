793623a7f7cbb2e4e645dc215835c2364bbfc2e5
AndrewMallach <andrew.mallach1@marist.edu>
2022-01-29 23:34:54 -0500

    sword animation
    base animation for sword

--- DegenJam/Assets/playerSword.cs
+++ DegenJam/Assets/playerSword.cs
@@ -4,23 +4,43 @@ using UnityEngine;
 
 public class playerSword : MonoBehaviour
 {
+    private bool swordSwinging;
+    public Animation anim;
     // Start is called before the first frame update
     void Start()
     {
-        
+        swordSwinging = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (swordSwinging == false)
+        {
+            
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            swordSwinging = true;
+            animation["anim"].wrapMode = WrapMode.Once;
+            animation.Play("anim");
+        }
     }
 
     void OnTriggerEnter2D(Collider2D col)
     {
         if(col.gameObject.tag == "enemy")
-        { 
-            Destroy(col.gameObject);
+        {
+            if (swordSwinging == true)
+            {
+                Destroy(col.gameObject);
+
+            }
+            
+            if (swordSwinging == false)
+            {
+                return;
+            }
         }
     }
 }
